9fad4ff1207aa4bf4fa8835ddfc0c1d49b5e0909
Leo Tenenbaum <pommicket@gmail.com>
2019-07-09 16:05:16 +1000

    Mostly finished game... maybe I'll add some music/sfx

--- mainmenu/main_menu.go
+++ mainmenu/main_menu.go
@@ -7,11 +7,10 @@ import (
 	"github.com/pommicket/box/state"
 )
 
-var start widgets.Button
-var levelEditor widgets.Button
-var exit widgets.Button
+var start, levelEditor, exit, inv1, inv2 widgets.Button
 var nextState state.State
-var shown bool
+var shown, c, h bool
+var sprC, sprH eng.Sprite
 
 func Load() {
 	start.LoadAll("start.bmp")
@@ -34,6 +33,22 @@ func Load() {
 	exit.OnClick = func() {
 		nextState = state.EXIT
 	}
+	inv1.LoadAll("invisible.bmp")
+	inv1.Scale = 4
+	inv1.Pos.SetParent(&exit.Pos, widgets.TOP_MIDDLE, widgets.BOTTOM_MIDDLE)
+	inv1.Pos.Move(0, 8)
+	inv1.OnClick = func() {
+		h = !h
+	}
+	inv2.LoadAll("invisible.bmp")
+	inv2.Scale = 4
+	inv2.Pos.SetParent(&inv1.Pos, widgets.TOP_MIDDLE, widgets.BOTTOM_MIDDLE)
+	inv2.Pos.Move(0, 8)
+	inv2.OnClick = func() {
+		c = !c
+	}
+	sprC.Load("c.bmp")
+	sprH.Load("h.bmp")
 	eng.OnKeyUp(keyUp)
 }
 
@@ -41,6 +56,8 @@ func Show() {
 	start.Show()
 	levelEditor.Show()
 	exit.Show()
+	inv1.Show()
+	inv2.Show()
 	nextState = state.MAIN_MENU
 	shown = true
 }
@@ -49,6 +66,8 @@ func Hide() {
 	start.Hide()
 	levelEditor.Hide()
 	exit.Hide()
+	inv1.Hide()
+	inv2.Hide()
 	shown = false
 }
 
@@ -56,6 +75,12 @@ func Render() state.State {
 	start.Pos.Move(eng.Width()/2, 8)
 	eng.SetColor(common.Color1)
 	eng.Clear()
+	if c {
+		sprC.Render(eng.Width()/2-sprC.Width*4, 400, 8)
+	}
+	if h {
+		sprH.Render(eng.Width()/2-sprC.Width*4+172, 365, 4)
+	}
 	return nextState
 }
 
